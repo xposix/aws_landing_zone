e53ee2d2ff85311dcad4f9a371a5490332177102
Ruben Sancho <17210993+xposix@users.noreply.github.com>
2020-08-19 23:34:29 +0100

    Fixing bug for mini admin role

--- modules/default_iam/role_aws_mini_admin_access.tf
+++ modules/default_iam/role_aws_mini_admin_access.tf
@@ -1,6 +1,6 @@
 ## Create the role and assign assume policy
 resource "aws_iam_role" "aws_mini_admin_access" {
-  count                = contains(var.roles, "aws_admin_access") ? 1 : 0
+  count                = contains(var.roles, "aws_mini_admin_access") ? 1 : 0
   name                 = "aws_mini_admin_access"
   assume_role_policy   = data.aws_iam_policy_document.saml_federated_and_api_access_assume.json
   max_session_duration = var.max_session_duration
