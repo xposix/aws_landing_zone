53bdce5d07beb1b807169f73b59461ad381a87cb
Ruben Sancho <ruben@deployferry.io>
2023-12-23 01:17:39 +0100

    Remove bucket configuration from backend.tf files as Makefiles do it all now

--- accounts/network/backend.tf
+++ accounts/network/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    # TO_FILL
     region         = "eu-west-1"
-    bucket         = "{COMPANY_PREFIX}-terraform-state-landing-zone-aws"
     key            = "github.com/xposix/aws_landing_zone/accounts/network/terraform.tfstate"
     dynamodb_table = "terraform_locks_landing_zone_aws"
     encrypt        = true
