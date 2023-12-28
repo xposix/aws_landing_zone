1fe90850e2de8907d167b33db22740a9770a8b34
Ruben Sancho <ruben@deployferry.io>
2023-12-28 12:14:43 +0000

    Fix s3 bucket in template

--- accounts/template/account/s3.tf
+++ accounts/template/account/s3.tf
@@ -1,7 +1,16 @@
-module "terraform-project-state-buckets" {
-  source             = "../../modules/tfstate_s3_bucket"
+module "terraform-project-state-buckets-v2" {
+  source = "../../modules/tfstate_s3_bucket_v2"
+
+  providers = {
+    aws.management = aws.management
+    aws.backup     = aws.backup
+  }
+
   primary_region     = var.primary_region
   backup_region      = var.secondary_region
-  local_account_name = local.account_name
   tags               = var.tags
+  local_account_name = local.account_name
+  bucket_purpose     = "local-projects"
+  enable_kms         = false
+
 }
